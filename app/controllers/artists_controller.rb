0ffd60e45e6122537745e95febc2bbef4a96d17b
sierraskyewallace <sskwallace@gmail.com>
2021-02-17 14:59:37 -0800

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,2 @@
 class ArtistsController < ApplicationController
-  get '/artists' do 
-    @artists = Artist.all 
-    erb :'/artists/index'
-end
-
-get '/artists/:slug' do 
-    slug = params[:slug]
-    @artist = Artist.find_by_slug(slug)
-    @songs = @artist.songs
-    @genres = @artist.genres 
-    erb :'/artists/show'
-end
 end
