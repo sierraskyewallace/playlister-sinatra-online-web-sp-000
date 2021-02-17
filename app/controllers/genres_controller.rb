8219cf73fe13fa493a9d3d37527559658891733e
sierraskyewallace <sskwallace@gmail.com>
2021-02-17 15:45:11 -0800

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,10 +3,10 @@
     get '/genres' do
       @genres = Genre.all
       erb :'genres/index'
-    end
-  
-    get '/genres/:slug' do
+  end
+
+  get '/genres/:slug' do
       @genre = Genre.find_by_slug(params[:slug])
-      erb :'/genres/show'
-    end
+      erb :'genres/show'
+  end
 end
