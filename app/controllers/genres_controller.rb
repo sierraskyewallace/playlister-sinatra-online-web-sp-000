e97752c48218762559c38981a083aba6fd3a5df6
sierraskyewallace <sskwallace@gmail.com>
2021-02-17 16:07:16 -0800

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,12 +1,12 @@
-  class GenresController < Sinatra::Base
-
-    get '/genres' do
-      @genres = Genre.all
-      erb :'genres/index'
+require 'rack-flash'
+class GenresController < ApplicationController
+  get '/genres' do
+    @genres = Genre.all
+    erb :'genres/index'
   end
 
   get '/genres/:slug' do
-      @genre = Genre.find_by_slug(params[:slug])
-      erb :'genres/show'
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show'
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,16 +8,43 @@ class SongsController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
- # get songs - show songs 
+ get '/songs' do 
+  @songs = Song.all 
+  erb :'songs/index'
+ end
 
- # get songs/new - shows all artists and genres 
+ get '/songs/new' do
+  erb :'/songs/new'
+end 
 
- # get song/sluhg - sgoifies songsd by params 
+get '/songs/:slug' do
+  @song = Song.find_by_slug(params[:slug])
+  erb :'songs/show'
+end
 
- # get songs/slugedit - finds song by slug params and shows genres
+post '/songs' do
+  @song = Song.create(params[:song])
+  @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  @song.genre_ids = params[:genres]
+  @song.save
 
- # patch song/slug - shows and updates songs 
+  flash[:message] = "Successfully created song."
+  redirect("/songs/#{@song.slug}")
+end
 
- # post songs - creates song and redirects to songs page 
+get '/songs/:slug/edit' do
+  @song = Song.find_by_slug(params[:slug])
+  erb :'songs/edit'
+end
 
+patch '/songs/:slug' do
+  @song = Song.find_by_slug(params[:slug])
+  @song.update(params[:song])
+  @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+  @song.genre_ids = params[:genres]
+  @song.save
+
+  flash[:message] = "Successfully updated song."
+  redirect("/songs/#{@song.slug}")
+end
 end
